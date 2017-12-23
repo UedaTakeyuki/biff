d5a8b117056a99157f69b0f2955db7cbe723ebe4
Takeyuki UEDA <ueda@latelierdueda.com>
2017-12-23 14:23:05 +0900

    use core implementation

--- send.php
+++ send.php
@@ -1,30 +1,6 @@
 <?php
-// If you are using Composer (recommended)
 require 'vendor/autoload.php';
-require 'sc.php';
-
-$name = "上田さん";
-$address = ADDRESS_UEDA;
-switch ($_POST['to']){
-	case "yamazaki":
-	  $name = "山崎様";
-	  $address = ADDRESS_YAMAZAKI;
-	  break;
-}
-$string = $name."\n";
-$string = $string.$_POST['now']."に、書類が届きました\n";
-$string = $string."http://titurel.uedasoft.com/biff/uploads/".$_POST['filename'];
-
-$from = new SendGrid\Email("配達通知", "biff@uedasoft.com");
-$subject = "書類がとどいています";
-$to = new SendGrid\Email($name, $address);
-$content = new SendGrid\Content("text/plain", $string);
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-
-$apiKey = SENDGRID_API_KEY;
-$sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-print_r($response->headers());
-echo $response->body();
+require_once 'sc.php';
+$addresses = $addresses_product;
+require_once 'send.core.php'
+?>
