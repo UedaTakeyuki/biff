63854ccafb7b2399c93e2ffee05c28730fcf86f4
Takeyuki UEDA <ueda@latelierdueda.com>
2017-12-23 16:38:35 +0900

    indent

--- view_notify_menu.js
+++ view_notify_menu.js
@@ -1,22 +1,22 @@
-        var app = new Vue(
-          {
-            el: "#view-notify-menu",
-            data: {
-            },
-            methods: {
-              sendnotification: function(to,now,filename){
-                $.ajax({
-                  type: "POST",
-                  url: gIni.sendscript_url,
-                  data: {
-                    to: to,
-                    now: now,
-                    filename: filename,
-                  },
-                  dataType: "json",
-                })
-              }
-            }
-          }
-        )
+var app = new Vue(
+  {
+    el: "#view-notify-menu",
+    data: {
+    },
+    methods: {
+      sendnotification: function(to,now,filename){
+        $.ajax({
+          type: "POST",
+          url: gIni.sendscript_url,
+          data: {
+            to: to,
+            now: now,
+            filename: filename,
+          },
+          dataType: "json",
+        })
+      }
+    }
+  }
+)
 
